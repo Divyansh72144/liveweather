7cbcdc9f3578ac46d5f2552c4e887cb68a6538f9
divyansh72144 <divyansh72144@gmail.com>
2025-12-27 12:44:19 +0200

    changed variable name

--- api/prewarm-cache.js
+++ api/prewarm-cache.js
@@ -3,8 +3,8 @@ import { fetchWithRetry, processHourlyData } from '../backend/utils/fetchWithRet
 import { Redis } from '@upstash/redis'
 
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN,
 })
 
 export default async function handler(req, res) {
